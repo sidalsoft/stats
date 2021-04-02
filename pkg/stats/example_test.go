278d2cbc75c8d6d3d2ccde31631c9b6ace31e590
Saidali Saburov <ssaburov@velox.biz>
2021-04-02 21:58:03 +0500

    -init

---
+++ pkg/stats/example_test.go
@@ -0,0 +1,48 @@
+package stats
+
+import (
+	"fmt"
+	"github.com/sidalsoft/bank/v2/pkg/bank/types"
+)
+
+func ExampleAvg() {
+	payments := []types.Payment{
+		{
+			Amount: 1_000_00,
+		},
+		{
+			Amount: 1_000_00,
+		},
+		{
+			Amount: 1_000_00,
+		},
+		{
+			Amount: 1_000_00,
+		},
+	}
+	fmt.Println(Avg(payments))
+	//Output: 100000
+}
+
+func ExampleTotalInCategoryAvg() {
+	payments := []types.Payment{
+		{
+			Amount:   2_000_00,
+			Category: "salom",
+		},
+		{
+			Amount:   1_000_00,
+			Category: "alek",
+		},
+		{
+			Amount:   5_000_00,
+			Category: "salom",
+		},
+		{
+			Amount:   1_000_00,
+			Category: "paka",
+		},
+	}
+	fmt.Println(TotalInCategory(payments, "salom"))
+	//Output: 700000
+}
---
+++ pkg/stats/stats.go
@@ -0,0 +1,22 @@
+package stats
+
+import "github.com/sidalsoft/bank/v2/pkg/bank/types"
+
+func Avg(payments []types.Payment) types.Money {
+	sum := types.Money(0)
+	for _, p := range payments {
+		sum += p.Amount
+	}
+	return sum / types.Money(len(payments))
+}
+
+func TotalInCategory(payments []types.Payment, category types.Category) types.Money {
+	sum := types.Money(0)
+	for _, p := range payments {
+		if p.Category != category {
+			continue
+		}
+		sum += p.Amount
+	}
+	return sum
+}
